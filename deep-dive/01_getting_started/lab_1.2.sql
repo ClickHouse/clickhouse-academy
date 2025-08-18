ff0141f8e88e832ba58907a48a8540a319706404
MichelleBaird-Clickhouse <michelle.baird@clickhouse.com>
2025-08-18 13:49:32 -0400

    Update lab_1.2.sql
    updating lab to make oss friendly in response to this error message from a user https://github.com/ClickHouse/clickhouse-academy/issues/39

--- deep-dive/01_getting_started/lab_1.2.sql
+++ deep-dive/01_getting_started/lab_1.2.sql
@@ -41,9 +41,31 @@ ENGINE = MergeTree
 PRIMARY KEY date;
 
 -- Step 6
+-- If you are using Clickhouse Cloud, run this query to insert your data into uk_prices_1
 INSERT INTO uk_prices_1
     SELECT * 
     FROM s3('https://learn-clickhouse.s3.us-east-2.amazonaws.com/uk_property_prices/uk_prices.csv.zst');
+
+-- If you are using the Clickhouse OSS, run this query instead
+NSERT INTO uk_prices_1
+SELECT
+    c1 as id,                 
+    c2 as price,        
+    parseDateTimeBestEffortOrNull(c3) AS date,
+    c4 as postcode,     
+    c5 as type,         
+    c6 as is_new,      
+    c7 as duration,    
+    c8 as addr1,
+    c9 as addr2,
+    c10 as street,
+    c11 as locality,
+    c12 as town,
+    c13 as district,
+    c14 as county,
+    c15 as column15,
+    c16 as column16
+FROM s3('https://learn-clickhouse.s3.us-east-2.amazonaws.com/uk_property_prices/uk_prices.csv.zst');
 
 -- Step 7
 SELECT count() FROM uk_prices_1;
