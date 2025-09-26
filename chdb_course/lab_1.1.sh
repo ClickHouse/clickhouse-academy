abe0fe0c68b5ff5de8d304afc3aafbc1058d84dc
MichelleBaird-Clickhouse <michelle.baird@clickhouse.com>
2025-09-26 14:40:58 -0400

    Update and rename lab_1.1.sql to lab_1.1.sh

---
+++ chdb_course/lab_1.1.sh
@@ -0,0 +1,9 @@
+# Lab 1.1
+# it's recommended that you start by creating a virtual environment
+python -m venv .venv
+source .venv/bin/activate
+
+# then install your packages
+pip install chdb ipython pandas pyarrow jupyter notebook jupyterlab requests jupysql
+
+
